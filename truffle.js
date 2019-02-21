ef79737133a6de9b6dd5b70c3ed2946caf9943e2
Max Plank <coolhomenow@gmail.com>
2019-02-21 18:23:06 +0600

    message

--- truffle.js
+++ truffle.js
@@ -1,24 +1,11 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var infura_apikey = "XXXXXX";
-var mnemonic =
-  "brown grit awful puppy crouch picnic annual street also rich business marriage";
-
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      host: "127.0.0.1",
+      port: 7545,
       network_id: "*" // Match any network id
-    },
-    ropsten: {
-      provider: new HDWalletProvider(
-        mnemonic,
-        "https://ropsten.infura.io/v3/aa53b4a238d840a38cecf616102aa6b3"
-      ),
-      gas: 5000000,
-      gasPrice: 10000000000,
-      network_id: 3
     }
   }
 };
